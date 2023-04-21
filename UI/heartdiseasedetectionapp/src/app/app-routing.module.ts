7a646aa8a5001d572f5dd0a3d24db15d91200d48
yagyandatta <yagyandattamurmu@gmail.com>
2023-04-21 11:21:10 +0530

    heart disease detected added

--- UI/heartdiseasedetectionapp/src/app/app-routing.module.ts
+++ UI/heartdiseasedetectionapp/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { HeaderComponent } from './header/header.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 const routes: Routes = [
  { path:'home', component : FormComponent},
  {path :'header',component: HeaderComponent},
  {path:'' , redirectTo:'/home' ,pathMatch:'full'},
- {path:'**', component:FormComponent }
+ {path:'**', component:FormComponent },
+ {path: 'chatbot', component: ChatbotComponent }
+
 ];
 
 @NgModule({
